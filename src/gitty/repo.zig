909fbc1b8c5362cc2612daf3b2c6f8444d31ec70
DonMatano <mwakimamatano@gmail.com>
2026-02-25 17:33:19 +0300

    test: use temp dir to set up git test

--- src/gitty/repo.zig
+++ src/gitty/repo.zig
@@ -9,41 +9,154 @@ const Blob = struct {
 };
 
 const repo_log = std.log.scoped(.Repo_Log);
+
 pub const Repo = struct {
-    arena: *std.heap.ArenaAllocator,
     git_path: []const u8,
     blobs: StringHashMap(Blob),
 
     pub fn init(git_path: []const u8, alloc: Alloc) !Repo {
-        var arena = std.heap.ArenaAllocator.init(alloc);
-
         return .{
-            .arena = &arena,
             .git_path = git_path,
-            .blobs = StringHashMap(Blob).init(arena.allocator()),
+            .blobs = StringHashMap(Blob).init(alloc),
         };
     }
 
     pub fn deinit(self: *Repo) void {
         self.blobs.deinit();
-        self.arena.deinit();
     }
 
-    pub fn parseRepoBlobs(self: *Repo) !void {
-        const alloc = self.arena.allocator();
+    pub fn parseRepoBlobs(self: *Repo, alloc: Alloc) !void {
         const obj_path = try std.fmt.allocPrint(alloc, "{s}/objects", .{self.git_path});
+        defer alloc.free(obj_path);
         var dir = try std.fs.cwd().openDir(obj_path, .{ .iterate = true });
         var walker = try dir.walk(alloc);
-        // defer walker.deinit();
+        defer walker.deinit();
         while (try walker.next()) |entry| {
-            repo_log.debug("{s}: {s}", .{ entry.basename, @tagName(entry.kind) });
+            repo_log.debug("{s}: {s}\n", .{ entry.basename, @tagName(entry.kind) });
+            std.debug.print("{s}: {s}\n", .{ entry.basename, @tagName(entry.kind) });
         }
     }
 
     // pub fn getRepoBlobs(self: *Repo) !StringHashMap(Blob) {}
 };
-test "ensure single commit " {
-    var repo = try Repo.init("testGit/singleCommit/git", testing.allocator);
-    try repo.parseRepoBlobs();
+pub const std_options = struct {
+    pub const log_level: std.log.Level = .debug;
+};
+
+fn testRunProcess(child: *std.process.Child) !void {
+    try child.spawn();
+    const exit_code = child.wait();
+    try std.testing.expectEqual(exit_code, std.process.Child.Term{ .Exited = 0 });
+}
+
+test "test empty repo " {
+    const testing_alloc = testing.allocator;
+    var temp_dir = testing.tmpDir(.{ .iterate = true, .access_sub_paths = true });
+    defer temp_dir.cleanup();
+    const temp_path = try temp_dir.dir.realpathAlloc(testing_alloc, ".");
+    defer testing_alloc.free(temp_path);
+    const argv = [_][]const u8{ "git", "init", temp_path };
+    var child = std.process.Child.init(&argv, testing_alloc);
+    try testRunProcess(&child);
+    var arena = std.heap.ArenaAllocator.init(testing.allocator);
+    defer arena.deinit();
+    const alloc = arena.allocator();
+    const full_path = try std.fmt.allocPrint(alloc, "{s}/.git", .{temp_path});
+    var repo = try Repo.init(full_path, alloc);
+    try repo.parseRepoBlobs(alloc);
+    repo.deinit();
+}
+test "repo with unadded test file" {
+    const testing_alloc = testing.allocator;
+    var temp_dir = testing.tmpDir(.{ .iterate = true, .access_sub_paths = true });
+    defer temp_dir.cleanup();
+    const temp_path = try temp_dir.dir.realpathAlloc(testing_alloc, ".");
+    defer testing_alloc.free(temp_path);
+    // const argv = [_][]const u8{ "git", "-C", temp_path, "init" };
+    const argv = [_][]const u8{ "git", "init", temp_path };
+    var child = std.process.Child.init(&argv, testing_alloc);
+    try testRunProcess(&child);
+    // Create File
+    var file = try temp_dir.dir.createFile("test.text", .{});
+    // Write to File
+    _ = try file.write("Testing.\n");
+    file.close();
+
+    var arena = std.heap.ArenaAllocator.init(testing.allocator);
+    defer arena.deinit();
+    const alloc = arena.allocator();
+    const full_path = try std.fmt.allocPrint(alloc, "{s}/.git", .{temp_path});
+    var repo = try Repo.init(full_path, alloc);
+    try repo.parseRepoBlobs(alloc);
+    repo.deinit();
+}
+
+test "repo with added test file" {
+    const testing_alloc = testing.allocator;
+    var temp_dir = testing.tmpDir(.{ .iterate = true, .access_sub_paths = true });
+    defer temp_dir.cleanup();
+    const temp_path = try temp_dir.dir.realpathAlloc(testing_alloc, ".");
+    defer testing_alloc.free(temp_path);
+    var arena = std.heap.ArenaAllocator.init(testing.allocator);
+    defer arena.deinit();
+    const alloc = arena.allocator();
+    const full_path = try std.fmt.allocPrint(alloc, "{s}/.git", .{temp_path});
+    const argv = [_][]const u8{ "git", "init", temp_path };
+    var child = std.process.Child.init(&argv, testing_alloc);
+
+    try testRunProcess(&child);
+    // Create File
+    var file = try temp_dir.dir.createFile("test.text", .{});
+    // Write to File
+    _ = try file.write("Testing.\n");
+    file.close();
+    const second_argv = [_][]const u8{ "git", "-C", temp_path, "add", "." };
+    child = std.process.Child.init(&second_argv, testing_alloc);
+
+    try testRunProcess(&child);
+
+    var repo = try Repo.init(full_path, alloc);
+    try repo.parseRepoBlobs(alloc);
+    repo.deinit();
+}
+
+test "repo with commited test file" {
+    const testing_alloc = testing.allocator;
+    var temp_dir = testing.tmpDir(.{ .iterate = true, .access_sub_paths = true });
+    defer temp_dir.cleanup();
+    const temp_path = try temp_dir.dir.realpathAlloc(testing_alloc, ".");
+    defer testing_alloc.free(temp_path);
+    var arena = std.heap.ArenaAllocator.init(testing.allocator);
+    defer arena.deinit();
+    const alloc = arena.allocator();
+    const full_path = try std.fmt.allocPrint(alloc, "{s}/.git", .{temp_path});
+    const argv = [_][]const u8{ "git", "init", temp_path };
+    var child = std.process.Child.init(&argv, testing_alloc);
+
+    try testRunProcess(&child);
+    // Create File
+    var file = try temp_dir.dir.createFile("test.text", .{});
+    // Write to File
+    _ = try file.write("Testing.\n");
+    file.close();
+    const second_argv = [_][]const u8{ "git", "-C", temp_path, "add", "." };
+    child = std.process.Child.init(&second_argv, testing_alloc);
+
+    try testRunProcess(&child);
+
+    const commit_argv = [_][]const u8{
+        "git",
+        "-C",
+        temp_path,
+        "commit",
+        "-m",
+        "\"Test commit\"",
+    };
+    child = std.process.Child.init(&commit_argv, testing_alloc);
+
+    try testRunProcess(&child);
+
+    var repo = try Repo.init(full_path, alloc);
+    try repo.parseRepoBlobs(alloc);
     repo.deinit();
 }
